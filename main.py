3e9fb805f3c6c4e7aa78c98dbae3e4b4d197bd80
L4fy <105458897+L4fy@users.noreply.github.com>
2024-11-11 18:30:00 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+expression1 = 2 * 2 + 2
+expression2 = 2 * (2 + 2)
+print(expression1)
+print(expression2)
+print(expression1 == expression2)
+
+# 4st program
+number = float('123.456')
+number *= 10
+print(int(number) % 10)
